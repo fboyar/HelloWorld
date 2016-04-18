54eaf1622400ef00526f8e9d0382aa16e32dd639
fboyar <farukboyar38@gmail.com>
2016-04-18 20:39:23 +0300

    Third Commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,8 +13,6 @@ public class Application {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
-        System.out.println("First Branch Commit!");
-        System.out.println("Sec Branch Commit!");
         
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
